8464e5bf58633b78a8a5d3aea3a724d032565671
Brian Thornburg <briant3275@gmail.com>
2022-01-18 01:29:17 -0600

    initial work on module 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      displayCityData: false,
+      cityData: []
+    }
+  }
+
+  handleClick = async () => {
+    console.log('button clicked');
+
+    try {
+//put url in get param
+    let cityResults = await axios.get();
+
+    console.log('results: ', cityResults.data.results);
+    this.setState({
+      displayCityData: true,
+      cityData: cityResults.data.results
+    })
+    } catch(error) {
+      console.log('oh no, our api pull, its broken');
+    }
+  }
+  render () {
+
+    let cityToRender = this.state.cityData.map((city,idx) =>(
+      <p key={idx}>{city.name}</p>
+    ))
+    return (
+      <>
+      <header>
+        <h1>City Explorer</h1>
       </header>
-    </div>
-  );
+          
+      <main>
+        <button onClick={this.handleClick}>Do a Thing</button>
+      </main>
+      </>
+    );
+  }
 }
-
 export default App;
